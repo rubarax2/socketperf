b6f9c6229abef69e7809f95e715536a8cb120632
ruben <rubarax@gmail.com>
2017-05-05 15:53:00 +0000

    new change

--- foo.c
+++ foo.c
@@ -3,33 +3,35 @@ initial content12
 more content
 
 and more
-foo.c  
-foo.c  
+foo.c
+foo.c
+
+content
 
 change
 change
 change
-foo.c  
+foo.c
 
 change
 
 change
 change
 <<<<<<< HEAD
-foo.c  
-foo.c  
-foo.c  
+foo.c
+foo.c
+foo.c
 =======
 
 change
 >>>>>>> task3
 
 new change
-foo.c  
-foo.c  
-foo.c  
-foo.c  
-foo.c 
+foo.c
+foo.c
+foo.c
+foo.c
+foo.c
 
 
 change
