7ee6daecba60fb43f0f41b5fb6c14bd749811e4d
ruben <rubarax@gmail.com>
2017-08-17 14:04:31 +0000

    change

--- qux.c
+++ qux.c
@@ -1,35 +1,35 @@
 initial content12
 
-more content
+more contentty
 
 and more
-foo.c  
-foo.c  
+foo.c
+foo.c
 
 change
 change
 change
-foo.c  
+foo.c
 
 change
 
 change
 change
 <<<<<<< HEAD
-foo.c  
-foo.c  
-foo.c  
+foo.c
+foo.c
+foo.c
 =======
 
 change
 >>>>>>> task3
 
 new change
-foo.c  
-foo.c  
-foo.c  
-foo.c  
-foo.c 
+foo.c
+foo.c
+foo.c
+foo.c
+foo.c
 
 
 change
@@ -37,4 +37,4 @@ change
 
 
 last change
-foo 
+foo
